df6f5e0e4739c26f8095b58b6105df615e1187e1
friskydingo0 <paper.pavan@gmail.com>
2020-02-01 00:32:01 -0500

    TileMap and Plugin delete
    1. Created a tile map for representing the world.
    2. Deleted github plugin.

---
+++ Assets/Scripts/TileLayout/TileMap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Grid where stuff grows, or things happen.
+/// </summary>
+public class TileMap : MonoBehaviour
+{
+    public Vector2 _mapSize;
+    public Vector3 _origin;
+    public TileCell _tilePrefab;
+
+    private List<List<TileCell>> m_tileCells;
+
+    private void Start()
+    {
+        m_tileCells = new List<List<TileCell>>();
+
+        // Init the grid layout. 
+        for (int i = 0; i < _mapSize.x; i++)
+        {
+            List<TileCell> row = new List<TileCell>();
+            for (int j = 0; j < _mapSize.y; j++)
+            {
+                TileCell newCell = Instantiate<TileCell>(_tilePrefab, _origin + new Vector3(i, 0, j), Quaternion.identity, transform);
+                row.Add(newCell);
+            }
+            m_tileCells.Add(row);
+        }
+    }
+
+}
+
+
