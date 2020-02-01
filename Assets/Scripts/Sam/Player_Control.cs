fe8b4b90da2303b18e40c210c738538c849131ae
Sam Jianghao Hu <jh4828@nyu.edu>
2020-02-01 12:33:16 -0500

    Improved player input control

--- Assets/Scripts/Sam/Player_Control.cs
+++ Assets/Scripts/Sam/Player_Control.cs
@@ -17,24 +17,28 @@ public class Player_Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 velo = Vector3.zero;
+        //------------------------ Old Rigid Method --------------------
+        //Vector3 velo = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            velo = new Vector3(1, velo.y, velo.z);
-        }else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            velo = new Vector3(-1, velo.y, velo.z);
-        }
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    velo = new Vector3(1, velo.y, velo.z);
+        //}else if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    velo = new Vector3(-1, velo.y, velo.z);
+        //}
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            velo = new Vector3(velo.x, velo.y, 1);
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            velo = new Vector3(velo.x, velo.y, -1);
-        }
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    velo = new Vector3(velo.x, velo.y, 1);
+        //}
+        //else if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    velo = new Vector3(velo.x, velo.y, -1);
+        //}
+        //-----------------------------------------------------------------
+
+        Vector3 velo = new Vector3(Input.GetAxis("Horizontal"), myrgbody.velocity.y, Input.GetAxis("Vertical"));
 
         myrgbody.velocity = velo * moveSpd * Time.deltaTime;
     }
