40fd3627766cb9e7eaf495c6adc6ff3d58f6aab9
Sam Jianghao Hu <jh4828@nyu.edu>
2020-02-01 14:34:47 -0500

    Implemented turning mechanics and isometric movements

--- Assets/Scripts/Sam/Player_FartSystem.cs
+++ Assets/Scripts/Sam/Player_FartSystem.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Player_FartSystem : MonoBehaviour
 {
     public float fartValue; // How much cloud density does one fart increase.
-    public float fartInterval; // How long is the arbitrary gay between each fart. 
+    public float fartInterval; // How long is the arbitrary gay between each fart.
+    public float fartPushForce; // A small force that the fart applies to player.
 
     private float fartMeter = 10f; // How long can the angel keep farting, represented in seconds.
     private float fartTimer = 0f;
@@ -19,7 +20,7 @@ public class Player_FartSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("How long can I fart: " + fartMeter);
+        //Debug.Log("How long can I fart: " + fartMeter);
         ParticleSystem fartDust = transform.GetChild(0).GetComponent<ParticleSystem>();
         if (Input.GetKey(KeyCode.Space))
         {
@@ -38,6 +39,8 @@ public class Player_FartSystem : MonoBehaviour
                     fartDust.Play();
                 }
                 fartMeter -= Time.deltaTime;
+
+                GetComponent<Rigidbody>().velocity = GetComponent<Player_Control>().GetFaceDirect() * fartPushForce * Time.deltaTime;
             }
             else
             {
@@ -49,6 +52,12 @@ public class Player_FartSystem : MonoBehaviour
         {
             fartTimer = 0;
             fartDust.Stop();
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 
@@ -67,7 +76,7 @@ public class Player_FartSystem : MonoBehaviour
             TileCell cellToFartOn = TileMap.Instance.GetTileUnderPoint(transform.position);
             if (cellToFartOn != null)
             {
-                cellToFartOn._cloudDensity += fartValue;
+                cellToFartOn.AddFartCloud(fartValue);
             }
         }
     }
