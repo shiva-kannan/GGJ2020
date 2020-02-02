fc9e06f022c542b6031bb8156a492e736fd5960b
Shiva Kannan <sssk2006@gmail.com>
2020-02-02 12:21:17 -0500

    Sound track on off boolean

--- Assets/Scripts/MusicGenerator/SoundTrack.cs
+++ Assets/Scripts/MusicGenerator/SoundTrack.cs
@@ -28,6 +28,7 @@ public class SoundTrack : MonoBehaviour
 
     //the duration of a beat
     float secPerBeat;
+    public float playOneShotVolume=0.15f;
 
     //how much time (in seconds) has passed since the song started
     float dspSongTime;
@@ -35,6 +36,8 @@ public class SoundTrack : MonoBehaviour
     private TileMap tileMapObject;
     private Vector2 currentGridPositionPlayer1;
     private Vector2 currentGridPositionPlayer2;
+
+    public bool stopRandomGeneration = true;
 
     // Start is called before the first frame update
     void Start()
@@ -63,27 +66,30 @@ public class SoundTrack : MonoBehaviour
         //determine how many beats since the song started
         songPositionInBeats = (int)(songPosition / secPerBeat);
 
-        //Start playing the base track when the game starts
-        if (baseTrackOn){
-            if(songPositionInBeats % 4 == 0 && songPositionInBeats !=lastFrameSongInBeats){
-                //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
-                playOneShot(baseTrackClipList[0], baseTrack[0], 0);
-                
-            }
-            if((songPositionInBeats + 2) % 4 == 0 && songPositionInBeats !=lastFrameSongInBeats){
-                //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
-                playOneShot(baseTrackClipList[0], baseTrack[0], 2);
-            }
-            if((songPositionInBeats + 1) % 4 == 0 && songPositionInBeats !=lastFrameSongInBeats){
-                //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
-                playOneShot(baseTrackClipList[0], baseTrack[0], 1);
-            }
-            // For the bass track
-            if((songPositionInBeats) % 8 == 0 && songPositionInBeats !=lastFrameSongInBeats){
-                //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
-                playOneShot(baseTrackClipList[0], baseTrack[0], 8);
+        if(!stopRandomGeneration){
+            //Start playing the base track when the game starts
+            if (baseTrackOn){
+                if(songPositionInBeats % 4 == 0 && songPositionInBeats !=lastFrameSongInBeats){
+                    //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
+                    playOneShot(baseTrackClipList[0], baseTrack[0], 0);
+                    
+                }
+                if((songPositionInBeats + 2) % 4 == 0 && songPositionInBeats !=lastFrameSongInBeats){
+                    //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
+                    playOneShot(baseTrackClipList[0], baseTrack[0], 2);
+                }
+                if((songPositionInBeats + 1) % 4 == 0 && songPositionInBeats !=lastFrameSongInBeats){
+                    //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
+                    playOneShot(baseTrackClipList[0], baseTrack[0], 1);
+                }
+                // For the bass track
+                if((songPositionInBeats) % 8 == 0 && songPositionInBeats !=lastFrameSongInBeats){
+                    //baseTrack.PlayOneShot(baseTrackClip, 0.4f);
+                    playOneShot(baseTrackClipList[0], baseTrack[0], 8);
+                }
             }
         }
+        
 
         lastFrameSongInBeats = songPositionInBeats;
 
@@ -94,14 +100,14 @@ public class SoundTrack : MonoBehaviour
         baseTrackOn = true;
         drumTrack.Play();
         //bassAudioSource.clip = baseTrackClipList[0];
-        bassAudioSource.Play();
+        //bassAudioSource.Play();
     }
 
     void playOneShot(AudioClip a, AudioSource s, int beatPosition){
         
         // For every 0th beat
         if(beatPosition == 0) {
-            s.PlayOneShot(a, 0.2f);
+            s.PlayOneShot(a, playOneShotVolume);
         } // Always play the base track
         else if (tileMapObject.GetTileUnderPoint(player1.transform.position) != null && 
         tileMapObject.GetTileUnderPoint(player2.transform.position) != null){
@@ -111,18 +117,18 @@ public class SoundTrack : MonoBehaviour
             // If position is even
             if (beatPosition == 2){
                 if((int)currentGridPositionPlayer2[0] % 2 == 0){
-                    baseTrack[(int)currentGridPositionPlayer2[0]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer2[0]], 0.2f);
+                    baseTrack[(int)currentGridPositionPlayer2[0]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer2[0]], playOneShotVolume);
                 }
                 if(currentGridPositionPlayer2[1] % 2 == 0){
-                    baseTrack[(int)currentGridPositionPlayer2[1]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer2[1]], 0.2f);
+                    baseTrack[(int)currentGridPositionPlayer2[1]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer2[1]], playOneShotVolume);
                 }
             }
             if (beatPosition == 1){
                 if((int)currentGridPositionPlayer1[0] % 2 == 1){
-                    baseTrack[(int)currentGridPositionPlayer1[0]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer1[0]], 0.2f);
+                    baseTrack[(int)currentGridPositionPlayer1[0]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer1[0]], playOneShotVolume);
                 }
                 if(currentGridPositionPlayer1[1] % 2 == 1){
-                    baseTrack[(int)currentGridPositionPlayer1[1]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer1[1]], 0.2f);
+                    baseTrack[(int)currentGridPositionPlayer1[1]].PlayOneShot(baseTrackClipList[(int)currentGridPositionPlayer1[1]], playOneShotVolume);
                 }
             }
             // if(beatPosition == 8){
@@ -149,5 +155,12 @@ public class SoundTrack : MonoBehaviour
     //     if (tileMapObject.GetTileUnderPoint(player.transform.position) != null){
     //         currentGridPosition = tileMapObject.GetTileUnderPoint(player.transform.position).pGridPos;
     //     }        
+    // }
+    // public void stopRandomSoundTrackGeneration(){
+    //     stopRandomGeneration = true;
+    // }
+
+    // public void startRandomGeneration(){
+    //     stopRandomGeneration = false;
     // }
 }
